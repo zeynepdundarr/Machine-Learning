70cc60a1d4b62d750d9b033d50c91e6d85b1324b
zeynepdundarr <40126925+zeynepdundarr@users.noreply.github.com>
2021-05-04 02:34:47 +0300

    Colon Tissue Image Classification

---
+++ File_Rename.py
@@ -0,0 +1,11 @@
+import os
+
+# folder = "/Users/zeynepdundar/Desktop/dataset/train_balanced/train_balanced_--"
+# i = 89
+# print("Length is: ",len(os.listdir(folder)))
+# for filename in os.listdir(folder):
+#   file_and_no = "tr"+str(i)+"."+"jpg"
+#   os.rename(r'/Users/zeynepdundar/Desktop/dataset/train_balanced/train_balanced_2/'+filename,
+#             r'/Users/zeynepdundar/Desktop/dataset/train_balanced/train_balanced_2/'+file_and_no)
+#   print(file_and_no)
+#   i += 1
